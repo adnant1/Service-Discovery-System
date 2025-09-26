7c857a34a2209968ecdd11297b411ea19ce141ae
adnant1 <adnantayib8@gmail.com>
2025-09-26 17:12:02 -0400

    Created method signatures for the RedisRepository

--- src/main/java/com/github/adnant1/servicediscovery/redis/RedisRepository.java
+++ src/main/java/com/github/adnant1/servicediscovery/redis/RedisRepository.java
@@ -1,7 +1,11 @@
 package com.github.adnant1.servicediscovery.redis;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import com.github.adnant1.servicediscovery.registry.ServiceInstance;
 
 /**
  * Repository class for managing service registry data in Redis.
@@ -15,4 +19,46 @@ public class RedisRepository {
         this.redisTemplate = redisTemplate;
         this.ttlSeconds = ttlSeconds;
     }
-}
+
+    /**
+     * Saves a service instance to Redis with a TTL.
+     * 
+     * @param serviceName the name of the service
+     * @param instanceId the unique ID of the service instance
+     * @param ip the IP address of the service instance
+     * @param port the port number of the service instance
+     */
+    public void saveInstance(String serviceName, String instanceId, String ip, int port) {
+
+    } 
+
+    /**
+     * Deletes a service instance from Redis.
+     * 
+     * @param serviceName the name of the service
+     * @param instanceId the unique ID of the service instance
+     */
+    public void deleteInstance(String serviceName, String instanceId) {
+
+    }
+
+    /**
+     * Retrieves all instances of a given service from Redis.
+     * 
+     * @param serviceName the name of the service
+     * @return a list of type ServiceInstance
+     */
+    public List<ServiceInstance> getInstances(String serviceName) {
+
+    }
+
+    /**
+     * Refreshes the TTL of a service instance in Redis.
+     * 
+     * @param serviceName the name of the service
+     * @param instanceId the unique ID of the service instance
+     */
+    public void refreshTtl(String serviceName, String instanceId) {
+
+    }
+}   
