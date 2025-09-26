5ae9e67b721b4b3ac27d079fd90376c76c6a8320
adnant1 <adnantayib8@gmail.com>
2025-09-25 23:47:12 -0400

    Configure Redis ttl time

--- src/main/java/com/github/adnant1/servicediscovery/redis/RedisRepository.java
+++ src/main/java/com/github/adnant1/servicediscovery/redis/RedisRepository.java
@@ -1,15 +1,18 @@
 package com.github.adnant1.servicediscovery.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * Repository class for managing service registry data in Redis.
  */
 public class RedisRepository {
- 
     private final StringRedisTemplate redisTemplate;
+    private final int ttlSeconds;
 
-    public RedisRepository(StringRedisTemplate redisTemplate) {
+    public RedisRepository(StringRedisTemplate redisTemplate, 
+                          @Value("${spring.ttl-seconds:30}") int ttlSeconds) {
         this.redisTemplate = redisTemplate;
+        this.ttlSeconds = ttlSeconds;
     }
 }
