c88916c356ab20824fc196ea5ad516d3f50e085d
adnant1 <adnantayib8@gmail.com>
2025-09-26 18:07:56 -0400

    Created config file to set profile test to active

--- src/main/java/com/github/adnant1/servicediscovery/ServicediscoveryApplication.java
+++ src/main/java/com/github/adnant1/servicediscovery/ServicediscoveryApplication.java
@@ -1,13 +1,19 @@
 package com.github.adnant1.servicediscovery;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+		/*
+		 * Run as a non-web application since this is a gRPC server
+		 */
+		new SpringApplicationBuilder(ServiceDiscoveryApplication.class)
+			.web(WebApplicationType.NONE)
+			.run(args);
 	}
 
 }
