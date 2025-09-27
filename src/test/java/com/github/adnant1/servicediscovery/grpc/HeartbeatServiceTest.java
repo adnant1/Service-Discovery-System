ed2aacb1703c0e2967ca8a036d53000cd7c99c4c
adnant1 <adnantayib8@gmail.com>
2025-09-27 18:29:38 -0400

    Test suite to test heartbeat gRPC method

---
+++ src/test/java/com/github/adnant1/servicediscovery/grpc/HeartbeatServiceTest.java
@@ -0,0 +1,18 @@
+package com.github.adnant1.servicediscovery.grpc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+import com.github.adnant1.servicediscovery.redis.RedisRepository;
+
+public class HeartbeatServiceTest {
+    
+    private RedisRepository redisRepository;
+    private RegistryServiceImpl service;
+
+    @BeforeEach
+    public void setUp() {
+        this.redisRepository = Mockito.mock(RedisRepository.class);
+        this.service = new RegistryServiceImpl(redisRepository);
+    }
+}
