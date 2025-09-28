10383d2f0f754bdaa2a0a0636fbae8a2eab9d97f
adnant1 <adnantayib8@gmail.com>
2025-09-27 20:11:58 -0400

    Created RedisConfig class with manual LettuceConnectionFactory configuration to properly read env variables

---
+++ src/main/java/com/github/adnant1/servicediscovery/redis/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.github.adnant1.servicediscovery.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * Configuration class for setting up Redis connection and StringRedisTemplate bean.
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        return new LettuceConnectionFactory(redisConfig);
+    }
+            
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
