24159e30fe967ccfe0212a8a9230302f1101ddf8
adnant1 <adnantayib8@gmail.com>
2025-09-28 19:10:29 -0400

    Class to get the gRPC port and create the NodeIdentityProvider bean

---
+++ src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
@@ -0,0 +1,11 @@
+package com.github.adnant1.servicediscovery.identity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IdentityConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(IdentityConfig.class);
+
+    
+}
