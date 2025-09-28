4365fb6e80f1797ad20310961dff49d306472b61
adnant1 <adnantayib8@gmail.com>
2025-09-28 19:13:11 -0400

    Create the NodeIdentityProvider bean

--- src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
+++ src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
@@ -3,6 +3,7 @@ package com.github.adnant1.servicediscovery.identity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import com.github.adnant1.servicediscovery.grpc.GrpcPortAllocator;
 
@@ -15,5 +16,10 @@ public class IdentityConfig {
         int port = GrpcPortAllocator.assignGrpcPort();
         logger.info("[BOOT] Assigned gRPC port: {}", port);
         return port;
+    }
+
+    @Bean
+    public NodeIdentityProvider nodeIdentityProvider(StringRedisTemplate redisTemplate, Integer grpcPort) {
+        return new NodeIdentityProvider(redisTemplate, grpcPort);
     }
 }
