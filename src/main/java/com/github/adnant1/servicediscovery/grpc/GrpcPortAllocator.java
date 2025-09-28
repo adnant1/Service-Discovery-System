e78dd3c41346a05f5d1fa725ed43da21602f3b85
adnant1 <adnantayib8@gmail.com>
2025-09-28 19:04:59 -0400

    Implemented method to assign the node a gRPC port

--- src/main/java/com/github/adnant1/servicediscovery/grpc/GrpcPortAllocator.java
+++ src/main/java/com/github/adnant1/servicediscovery/grpc/GrpcPortAllocator.java
@@ -8,5 +8,19 @@ public class GrpcPortAllocator {
 
     private static final int START_PORT = 50051;
 
+    /**
+     * Returns a gRPC port for the node to use.
+     * Increments from the starting port if the port is already in use.
+     * 
+     * @return an available gRPC port
+     */
+    public static int assignGrpcPort() {
+        int port = START_PORT;
+        while (!isFree(port)) {
+            port++;
+        }
+
+        return port;
+    }
     
 }
