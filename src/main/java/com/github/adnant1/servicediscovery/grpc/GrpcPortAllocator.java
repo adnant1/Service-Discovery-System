5ce6d1eb01c7f29c241e3e467043b9d057dcbace
adnant1 <adnantayib8@gmail.com>
2025-09-28 19:03:14 -0400

    Created GrpcPortAllocator class to assign the node a port

---
+++ src/main/java/com/github/adnant1/servicediscovery/grpc/GrpcPortAllocator.java
@@ -0,0 +1,12 @@
+package com.github.adnant1.servicediscovery.grpc;
+
+/**
+ * Handles gRPC port allocation for the service registry.
+ * Starts at the default port 50051 and increments if the port is in use.
+ */
+public class GrpcPortAllocator {
+
+    private static final int START_PORT = 50051;
+
+    
+}
