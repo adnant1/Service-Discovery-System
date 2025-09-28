54bac86577613363fa0c5e1bd3cf3f86d55689c8
adnant1 <adnantayib8@gmail.com>
2025-09-28 19:11:53 -0400

    Get the gRPC port from the GrpcPortAllocator

--- src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
+++ src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
@@ -2,10 +2,18 @@ package com.github.adnant1.servicediscovery.identity;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+
+import com.github.adnant1.servicediscovery.grpc.GrpcPortAllocator;
 
 public class IdentityConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(IdentityConfig.class);
 
-    
+    @Bean
+    public Integer grpcPort() {
+        int port = GrpcPortAllocator.assignGrpcPort();
+        logger.info("[BOOT] Assigned gRPC port: {}", port);
+        return port;
+    }
 }
