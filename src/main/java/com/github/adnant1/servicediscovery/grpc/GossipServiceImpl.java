24a825e401ca15b64a0df7dddeecf4af1f138c15
adnant1 <adnantayib8@gmail.com>
2025-09-28 21:21:08 -0400

    Test implementation of sync method; log message + ACK response

---
+++ src/main/java/com/github/adnant1/servicediscovery/grpc/GossipServiceImpl.java
@@ -0,0 +1,27 @@
+package com.github.adnant1.servicediscovery.grpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.adnant1.servicediscovery.registry.GossipRequest;
+import com.github.adnant1.servicediscovery.registry.GossipResponse;
+import com.github.adnant1.servicediscovery.registry.GossipServiceGrpc;
+
+import io.grpc.stub.StreamObserver;
+
+public class GossipServiceImpl extends GossipServiceGrpc.GossipServiceImplBase {
+
+    private static final Logger logger = LoggerFactory.getLogger(GossipServiceImpl.class);
+    
+    @Override
+    public void sync(GossipRequest request, StreamObserver<GossipResponse> responseObserver) {
+        // For now log sender + how many services and return ACK
+        logger.info("Received gossip sync with {} instances", request.getInstancesCount());
+        GossipResponse response = GossipResponse.newBuilder()
+                .setSuccess(true)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+    
+}
