b542aae904976dc89ffc3a1d4f38921411802291
adnant1 <adnantayib8@gmail.com>
2025-09-28 20:36:02 -0400

    Defaulted to port 50051

--- src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
+++ src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
@@ -1,23 +1,15 @@
 package com.github.adnant1.servicediscovery.identity;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import com.github.adnant1.servicediscovery.grpc.GrpcPortAllocator;
-
 @Configuration
 public class IdentityConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(IdentityConfig.class);
-
     @Bean
     public Integer grpcPort() {
-        int port = GrpcPortAllocator.assignGrpcPort();
-        logger.info("[BOOT] Assigned gRPC port: {}", port);
-        return port;
+        return 50051;
     }
 
     @Bean
