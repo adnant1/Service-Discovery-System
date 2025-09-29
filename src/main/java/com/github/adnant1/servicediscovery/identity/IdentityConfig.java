de8d460535d8e02894b9dbe95fb7127c33937af6
adnant1 <adnantayib8@gmail.com>
2025-09-28 20:16:21 -0400

    Added @Configuration annotation to IdentityConfig

--- src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
+++ src/main/java/com/github/adnant1/servicediscovery/identity/IdentityConfig.java
@@ -3,10 +3,12 @@ package com.github.adnant1.servicediscovery.identity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import com.github.adnant1.servicediscovery.grpc.GrpcPortAllocator;
 
+@Configuration
 public class IdentityConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(IdentityConfig.class);
