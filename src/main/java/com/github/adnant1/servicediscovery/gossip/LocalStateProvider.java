df749aae1e4183559105d40fda6fbbe8bb8ae6aa
adnant1 <adnantayib8@gmail.com>
2025-09-29 23:18:26 -0400

    Remove ServiceInstanceSerializer bean

--- src/main/java/com/github/adnant1/servicediscovery/gossip/LocalStateProvider.java
+++ src/main/java/com/github/adnant1/servicediscovery/gossip/LocalStateProvider.java
@@ -3,16 +3,12 @@ package com.github.adnant1.servicediscovery.gossip;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
-import com.github.adnant1.servicediscovery.util.ServiceInstanceSerializer;
-
 @Component
 public class LocalStateProvider {
  
     private final StringRedisTemplate redisTemplate;
-    private final ServiceInstanceSerializer serializer;
 
-    public LocalStateProvider(StringRedisTemplate redisTemplate, ServiceInstanceSerializer serializer) {
+    public LocalStateProvider(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
-        this.serializer = serializer;
     }
 }
