406f75118e1dced3f16c722d8ce4e622c3350b2e
adnant1 <adnantayib8@gmail.com>
2025-09-30 15:40:33 -0400

    Enable scheduling in the main application class

--- src/main/java/com/github/adnant1/servicediscovery/ServicediscoveryApplication.java
+++ src/main/java/com/github/adnant1/servicediscovery/ServicediscoveryApplication.java
@@ -2,7 +2,9 @@ package com.github.adnant1.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 	public static void main(String[] args) {
